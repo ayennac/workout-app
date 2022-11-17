f97a95df6cdafc881a8f439b281dcb9dce8c2043
ayennac <ayennac@gmail.com>
2022-11-17 13:24:19 -0800

    added WorkoutDetails component and sytling

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,13 @@
 import {Link} from 'react-router-dom'
+//this link component handles the routing locally in the browser instead of requesting from the server
 
 const Navbar = () =>{
     return(
         <header>
         <div className = "container">
+            <Link to="/">
             <h1>Workout Buddy</h1>
+            </Link>
         </div>
         </header>
     )
