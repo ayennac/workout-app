40c743aa32799fc2bde4a69fbe99637add2be9b1
ayennac <ayennac@gmail.com>
2023-02-05 19:44:36 -0800

    created user model, controllers, and routes

--- backend/models/workout-model.js
+++ backend/models/workout-model.js
@@ -3,6 +3,24 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 // Mongoose Schema Types: https://mongoosejs.com/docs/schematypes.html
+// https://zellwk.com/blog/mongoose-population/
+/*
+So, separate collections are good if you need to select individual documents, 
+need more control over querying, or
+ have huge documents.
+
+Embedded documents are good when you want the entire document,
+ the document with a $slice of comments,
+  or with no comments at all.
+
+*/
+
+/* Example of a schema with embedded 
+subdocument:
+https://stackoverflow.com/questions/39596625/nested-objects-in-mongoose-schemas
+
+*/
+
 
 //Schema
 //defines the structure of the type of document in our datbase
